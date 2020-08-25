9be42e7e4c90706f0be51101131c6535ec385592
arsen <baregamyan@gmail.com>
2020-08-26 02:12:53 +0300

    Добавляет переименованные скрипты

--- js/Form.js
+++ js/Form.js
@@ -1,40 +1,45 @@
 'use strict';
+(function () {
 
-function Form() {
-  this.form = document.querySelector('.ad-form');
-  this.input = {
-    avatar: this.form.querySelector('#avatar'),
-    title: this.form.querySelector('#title'),
-    address: this.form.querySelector('#address'),
-    type: this.form.querySelector('#type'),
-    price: this.form.querySelector('#price'),
-    timein: this.form.querySelector('#timein'),
-    timeout: this.form.querySelector('#timeout'),
-    rooms: this.form.querySelector('#room_number'),
-    guests: this.form.querySelector('#capacity')
+  function Form() {
+    this.form = document.querySelector('.ad-form');
+    this.input = {
+      title: this.form.querySelector('#title'),
+      address: this.form.querySelector('#address'),
+      type: this.form.querySelector('#type'),
+      price: this.form.querySelector('#price'),
+      timein: this.form.querySelector('#timein'),
+      timeout: this.form.querySelector('#timeout'),
+      rooms: this.form.querySelector('#room_number'),
+      capacity: this.form.querySelector('#capacity')
+    };
+    this.sendButton = this.form.querySelector('.ad-form__submit');
+    this.disable();
+  }
+
+  Form.prototype.disable = function () {
+    this.fieldsets = this.form.querySelectorAll('fieldset');
+    this.form.classList.toggle('ad-form--disabled', true);
+
+    for (var i = 0; i < this.fieldsets.length; i++) {
+      var _fieldset = this.fieldsets[i];
+      _fieldset.setAttribute('disabled', true);
+    }
   };
-  this.disable();
-}
 
-Form.prototype.disable = function () {
-  this.fieldsets = this.form.querySelectorAll('fieldset');
-  this.form.classList.toggle('ad-form--disabled', true);
-  for (var i = 0; i < this.fieldsets.length; i++) {
-    var _fieldset = this.fieldsets[i];
-    _fieldset.setAttribute('disabled', true);
-  }
-};
+  Form.prototype.enable = function () {
+    this.form.classList.toggle('ad-form--disabled', false);
+    for (var i = 0; i < this.fieldsets.length; i++) {
+      var _fieldset = this.fieldsets[i];
+      _fieldset.removeAttribute('disabled');
+    }
+    var validity = new window.Validity(this.input);
+    validity.init();
+  };
 
-Form.prototype.enable = function () {
-  this.form.classList.toggle('ad-form--disabled', false);
-  for (var i = 0; i < this.fieldsets.length; i++) {
-    var _fieldset = this.fieldsets[i];
-    _fieldset.setAttribute('disabled', false);
-  }
-};
+  Form.prototype.setAddress = function (x, y) {
+    this.input.address.value = Math.floor(x) + ', ' + Math.floor(y);
+  };
 
-Form.prototype.setAddress = function (x, y) {
-  this.input.address.value = Math.floor(x) + ', ' + Math.floor(y);
-};
-
-window.Form = Form;
+  window.Form = Form;
+})();
--- js/utils.js
+++ js/utils.js
@@ -1,46 +1,49 @@
 'use strict';
+(function () {
 
-/** Кейкоды клавиш */
-var Keycode = {
-  ESC: 27,
-  ENTER: 13
-};
+  /** Кейкоды клавиш */
+  var Keycode = {
+    ESC: 27,
+    ENTER: 13
+  };
 
-/**
- * Возвращает случайное число.
- * @param {number} min - Минимально-возможное число.
- * @param {number} max - Максимально-возможное число.
- * @return {number}
- */
-function getRandomInit(min, max) {
-  var _min = min || 0;
-  var _max = max || 700;
-  var _rand = _min + Math.random() * (_max - _min);
-  return Math.round(_rand);
-}
-
-/**
- * Перемешивает элеметы в массиве и возвращает перемешанный массив.
- * @param {Array} array - Массив.
- * @return {Array}
- */
-function shuffleArray(array) {
-  var uniques = [];
-  var i = 0;
-  while (i < array.length) {
-    var _element = array[window.utils.randomInit(0, array.length - 1)];
-    if (!uniques.includes(_element)) {
-      uniques.push(_element);
-      i++;
-    }
+  /**
+   * Возвращает случайное число.
+   * @param {number} min - Минимально-возможное число.
+   * @param {number} max - Максимально-возможное число.
+   * @return {number}
+   */
+  function getRandomInit(min, max) {
+    var _min = min || 0;
+    var _max = max || 700;
+    var _rand = _min + Math.random() * (_max - _min);
+    return Math.round(_rand);
   }
-  return uniques;
-}
 
-window.utils = {
-  keycode: Keycode,
-  randomInit: getRandomInit,
-  shuffle: shuffleArray
-};
+  /**
+   * Перемешивает элеметы в массиве и возвращает перемешанный массив.
+   * @param {Array} array - Массив.
+   * @return {Array}
+   */
+  function shuffleArray(array) {
+    var uniques = [];
+    var i = 0;
+    while (i < array.length) {
+      var _element = array[window.utils.randomInit(0, array.length - 1)];
+      if (!uniques.includes(_element)) {
+        uniques.push(_element);
+        i++;
+      }
+    }
+    return uniques;
+  }
+
+  window.utils = {
+    keycode: Keycode,
+    randomInit: getRandomInit,
+    shuffle: shuffleArray
+  };
+
+})();
 
 
